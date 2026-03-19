ee01b2b6bf4425f56c2624eccee3fd3ab4f94774
Jimbopython <le.stoffers@gmail.com>
2026-03-19 07:23:47 +0100

    test: new github actions for vs and execute

---
+++ cpp/dummy/main.cpp
@@ -0,0 +1,18 @@
+#include <peak_icv/peak_icv.hpp>
+#include <iostream>
+
+int main()
+{
+    try
+    {
+        peak::icv::library::Init();
+
+        peak::icv::library::Exit();
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
